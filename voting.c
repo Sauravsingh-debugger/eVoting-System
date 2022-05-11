50e5df502e24a2f963b282255ad28f93aee15059
Saurav Singh <67595632+Sauravsingh-debugger@users.noreply.github.com>
2022-05-11 13:44:42 +0530

    update

--- voting.c
+++ voting.c
@@ -1,522 +1,106 @@
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-
-typedef struct  voter_information
+#include < stdio.h >
+int bjp=0, congress=0, aap=0, bsp=0;
+void electionResult()
 {
-
-    char id[10];
-    char name[20];
-    char birth_date[15];
-    char father_name[20];
-    char mother_name[20];
-    struct  voter_information *next;
-}node;
-
-node *head;
-
-
-int cunt1=0,cunt2=0,cunt3=0,cunt4=0,cunt5=0;
-void main_loges();
-void voter_insert(); 
-void voting();
-void admin();
-void show();
-void stop();
-void exi();
-void not_again();
-void winner();
-
-int main()
-{
-
-    system("clear");
-
-    printf("\n\n\n");
-    printf("------------WELCOME TO ONLINE VOTING SYSTEM--------------");
-
-    printf("-------****************************************----  \n\n\n\n\n");
-    printf("\t\t ---------Please Enter One(1) For logging Vote Main Loges -------\n\n\n");
-    int B;
-
-    scanf("%d",&B);
-
-    if(B==1)
+    int wonByVote;
+    if (bjp > congress && bjp > aap && bjp > bsp)
     {
-
-        main_loges();
+        printf("\n***BJP won the election***\n\n");
+        printf("Total vote of BJP : %d\n",bjp);
+        wonByVote=bjp-congress;
+        printf("BJP won by %d votes to Congress\n",wonByVote);
+        wonByVote=bjp-aap;
+        printf("BJP won by %d votes to AAP\n",wonByVote);
+        wonByVote=bjp-bsp;
+        printf("BJP won by %d votes to BSP\n",wonByVote);
     }
-
-    return 0;
+    else if (congress > aap && congress > bsp)
+    {
+        printf("\n***Congress won the election***\n\n");
+        printf("Total vote of Congress : %d\n",congress);
+        wonByVote=congress-bjp;
+        printf("Congress won by %d votes to BJP\n",wonByVote);
+        wonByVote=congress-aap;
+        printf("Congress won by %d votes to AAP\n",wonByVote);
+        wonByVote=congress-bsp;
+        printf("Congress won by %d votes to BSP\n",wonByVote);
+    }
+    else if (aap > bsp)
+    {
+        printf("\n***AAP won the election***\n\n");
+        printf("Total vote of AAP : %d\n",aap);
+        wonByVote=aap-congress;
+        printf("AAP won by %d votes to Congress\n",wonByVote);
+        wonByVote=aap-bjp;
+        printf("AAP won by %d votes to BJP\n",wonByVote);
+        wonByVote=aap-bsp;
+        printf("AAP won by %d votes to BSP\n",wonByVote);
+    }
+    else if(bjp == congress && bjp == aap && bjp == bsp)
+    {
+        printf("\nNo one won the election\n\n");
+        printf("            BJP---Congress---AAP---BSP\n");
+        printf("Total Vote   %d       %d        %d     %d\n",bjp,congress,aap,bsp);
+    }
+    else
+    {
+        printf("\n***BSP won the election***\n\n");
+        printf("Total vote of BSP : %d\n",bsp);
+        wonByVote=bsp-congress;
+        printf("BSP won by %d votes to Congress\n",wonByVote);
+        wonByVote=bsp-aap;
+        printf("BSP won by %d votes to AAP\n",wonByVote);
+        wonByVote=bsp-bjp;
+        printf("BSP won by %d votes to BJP\n",wonByVote);
+    }
 }
-
-
-void main_loges()
+void calculateVote(int vote)
 {
-system("clear");
-
-printf("          \t\t\t\t1.     FOR VOTE ENTRY      -----------|||\n");
-sleep(1);
-
-printf("          \t\t\t\t2      FOR ADMIN PANEL    ----------|||\n");
-sleep(1);
-
-printf("          \t\t\t\t3     FOR WINNER         ----------|||\n");
-sleep(1);
-
-printf("          \t\t\t\t4      FOR EXIT         ----------|||\n\n\n\n\n");
-sleep(1);
-
-printf("       \t----------------------So Please Enter (1) ----------------------\n\n\n");
-
-int T;
-scanf("%d",&T);
-
-if(T==1)
-{
-
-    voter_insert();
+    switch (vote)
+    {
+    case 1:
+        bjp+=1;
+        break;
+    case 2:
+        congress+=1;
+        break;
+    case 3:
+        aap+=1;
+        break;
+    case 4:
+        bsp+=1;
+        break;
+    }
 }
-
-if(T==2)
+void main()
 {
-
-    admin();
+    int choose;
+    
+    
+        printf("\n*********Welcome to the simple voting system project*********\n\n");
+        printf("                         Delhi election                        \n\n");
+        printf("*************************************************************\n");
+        printf("|           1.BJP             |          2.Congress         |\n");
+        printf("*************************************************************\n");
+        printf("|           3.APP             |          4.BSP              |\n");
+        printf("*************************************************************\n\n");
+    do
+    {    
+        printf("Press 1 to vote BJP\n");
+        printf("Press 2 to vote Congress\n");
+        printf("Press 3 to vote APP\n");
+        printf("Press 4 to vote BSP\n");
+        printf("Press 5 to show election result\n");
+        printf("Please choose : ");
+        scanf("%d", &choose);
+        if (choose==5)
+        {
+            electionResult();
+        }else
+        {
+            calculateVote(choose);            
+        } 
+        printf("\n");
+    } while (choose != 5);
 }
-
-if(T==3)
-{
-
-    winner();
-}
-if(T==4)
-{
-
-    exi();
-}
-}
-
-int cunt=0,count=0,R=3,voteId1=0,voteId2=0,voteId3=0,voteId4=0,voteId5=0;
-char a[1000];
-
-void voter_insert()
-{
-    node *temp;
-
-    char name[25],birth_date[15],f_name[25],m_name[25],id[10];
-    system("clear");
-
-    printf("\n\n\n\n");
-    printf("\tIF NATIONAL ID,YOUR NAME,BIRTH DATE,YOUR FATHER NAME AND MOTHER NAME MATCH YOU CAN GIVE VOTE OTHERWISE NOT\n\n");
-    sleep(1);
-
-    printf("\t\t\t IF YOU DO WRONG %d  TIMES , THE PROGRAM WILL BE STOP AUTOMATICALLY \n\n\n",R);
-    sleep(1);
-
-    printf("\t\tPLEASE ----,\n");
-    sleep(1);
-
-    printf("\t\t\t\t ENTER YOUR NATIONAL ID NUMBER  ");
-    scanf("%s",id);
-    printf("\t\t\t\tENTER YOUR NAME                 ");
-    scanf("%s",name);
-
-    printf("\t\t\t\tENTER YOUR BIRTH DATE           ");
-    scanf("%s",birth_date);
-
-    printf("\t\t\t\tENTER YOUR FATHER NAME          ");
-    scanf("%s",f_name);
-
-    printf("\t\t\t\tENTER YOUR MOTHER NAME          ");
-    scanf("%s",m_name);
-
-    temp =(node *)malloc (sizeof(node));
-    strcpy(temp->id,id);
-    strcpy(temp->name,name);
-    strcpy(temp->birth_date,birth_date);
-    strcpy(temp->father_name,f_name);
-    strcpy(temp->mother_name,m_name);
-    temp->next=NULL;
-    head=temp;
-    while(temp!=NULL)
-    {
-
-        cunt=0;
-        if(strcmp(temp->id,"10000001")==0 && strcmp(temp->name,"ramesh")==0 && strcmp(temp->birth_date,"19-06-1993")==0 && strcmp(temp->father_name,"suresh")==0 && strcmp(temp->mother_name,"vyomkesh")==0)
-            {
-
-            if(voteId1>0){
-                not_again();
-              break;
-           }
-            ++voteId1;
-
-           voting();
-
-
-
-            }
-    else if(strcmp(temp->id,"10000002")==0 && strcmp(temp->name,"maninder")==0 && strcmp(temp->birth_date,"19-05-1993")==0 && strcmp(temp->father_name,"mahesh")==0 && strcmp(temp->mother_name,"belari")==0)
-           {
-
-                if(voteId2>0){
-                    not_again();
-              break;
-           }
-           voteId2++;
-           voting();
-
-
-
-
-           }
-
-        else if(strcmp(temp->id,"10000003")==0 && strcmp(temp->name,"nitesh")==0 && strcmp(temp->birth_date,"19-04-1993")==0 && strcmp(temp->father_name,"kamlesh")==0 && strcmp(temp->mother_name,"aradhana")==0)
-          {
-
-                if(voteId3>0){
-                    not_again();
-              break;
-           }
-           voteId3++;
-
-           voting();
-
-
-
-           }
-
-
-           else if(strcmp(temp->id,"10000004")==0 && strcmp(temp->name,"abhishek")==0 && strcmp(temp->birth_date,"19-03-1993")==0 && strcmp(temp->father_name,"mihir")==0 && strcmp(temp->mother_name,"moksha")==0)
-           {
-
-                if(voteId4>0){
-                    not_again();
-              break;
-           }
-            voteId4++;
-           voting();
-
-
-
-
-           }
-
-
-           else if(strcmp(temp->id,"10000005")==0 && strcmp(temp->name,"vivek")==0 && strcmp(temp->birth_date,"19-02-1993")==0 && strcmp(temp->father_name,"gautam")==0 && strcmp(temp->mother_name,"shanu")==0)
-         {
-
-                if(voteId5>0){
-                    not_again();
-              break;
-           }
-            voteId5++;
-
-           voting();
-
-
-
-           }
-
-
-
-
-
-
-        else
-        {
-            R--;
-            count++;
-            if(count==3)
-            {
-                stop();
-                break;
-
-            }
-
-            printf("\n\n\n\n");
-            printf("\t\tYour voter ID or NAME or DATE OF BIRTH or FATHER'S  NAME is wrong \n\n");
-            sleep(300);
-
-            printf("\t\t\t\t\t Please Re Enter \n\n");
-            sleep(300);
-
-            system("pause");
-            main_loges();
-
-        }
-
-        temp=temp->next;
-
-
-    }
-
-    }
-
-    void voting()
-    {
-        system("clear");
-        printf("\n\n\n\n\n");
-        printf("\t\t             * * * * * *  List of the Candidates * * * * * *   \n\n\n");
-        sleep(1);
-
-        printf("\t\t\t Name - - - - - -- - - - - - - SYMBOL \n\n");
-        sleep(1);
-
-        printf ("\t\t\t  1. BJP              1. Boat\n");
-        sleep(1);
-
-        printf("\t\t\t   2. RJD              2.paddy\n");
-        sleep(1);
-
-         printf("\t\t\t  3. BBD              3.cycle\n");
-        sleep(1);
-
-         printf("\t\t\t  2. SSD              4.lamp\n");
-        sleep(1);
-
-         printf("\t\t\t  2. CPM              5. broom \n");
-        sleep(1);
-
-        int B,j;
-
-        printf("\t\t\t Please--, \n");
-        printf("\t\t\t\t Enter your choice   ");
-
-        for(j=1;j<=1;j++)
-        {
-            scanf("%d",&B);
-
-            if(B==1)
-            {
-
-                cunt1++;
-            }
-
-            if(B==2)
-            {
-
-                cunt2++;
-            }
-
-            if(B==3)
-            {
-
-                cunt3++;
-            }
-            if(B==4)
-            {
-                cunt4++;
-
-            }
-
-            if(B==5)
-            {
-
-                cunt5++;
-            }
-
-            if(B!=1 && B!=2 && B!=3 && B!=4 && B!=5)
-            {
-
-                printf("\t\t\t Your vote is invalid \n");
-                main_loges();
-            }
-
-        }
-
-        int R;
-        system("clear");
-
-        printf("\n\n\n\n");
-        printf("\t\t\t If you want to see present winner enter one(1) or \n\n\n \t\t\t\t\tZero(0) For Main Loges \n\n\n");
-        scanf("%d",&R);
-
-        if(R==1)
-        {
-            winner();
-
-        }
-
-        if(R!=1)
-        {
-
-            main_loges();
-        }
-
-
-    }
-
-    void admin()
-    {
-
-        int B;
-        printf("\n\n\n\n");
-
-        printf("\t\t\t ENter password to unlock Admin panel\n\n");
-
-        scanf("%d",&B);
-        if(B==10715)
-        {
-            show();
-
-        }
-
-        else
-        {
-
-        printf("Wrong password\n");
-        }
-
-    }
-
-
-    void show()
-    {
-        system("clear");
-        printf("\n\n\n");
-        printf("\t\t\t How many votes who get \n\n\n");
-        sleep(5);
-
-        printf(" BJP got  %d votes \n ",cunt1);
-        sleep(5);
-
-        printf(" RJD got  %d votes \n ",cunt2);
-        sleep(5);
-
-         printf(" BBD  got  %d votes \n ",cunt3);
-        sleep(5);
-
-         printf(" SSD got  %d votes \n ",cunt4);
-        sleep(5);
-
-         printf(" CPM got  %d votes \n ",cunt5);
-        sleep(5);
-
-        int R;
-        printf("\t\t\t Enter One(1) For Main Loges Or \n\n\n \t\t\t");
-
-        scanf("%d",&R);
-
-        if(R==1)
-        {
-
-            main_loges();
-        }
-        else{
-            exi();
-        }
-
-
-
-    }
-
-    void winner()
-    {
-
-        system("clear");
-        printf("\n\n\n\n");
-
-        if(cunt2<cunt1 && cunt3<cunt1 && cunt4<cunt1 && cunt5<cunt1)
-            printf("\t\t\t The present winner is BJP and he got %d votes\n\n\n\n",cunt1);
-
-        if(cunt1<cunt2 && cunt3<cunt2 && cunt4<cunt2 && cunt5<cunt2)
-            printf("\t\t\t The present winner is RJD and he got %d votes\n\n\n\n",cunt2);
-
-        if(cunt1<cunt3 && cunt2<cunt3 && cunt4<cunt3 && cunt5<cunt3)
-            printf("\t\t\t The present winner is BBD and he got %d votes\n\n\n\n",cunt3);
-
-        if(cunt1<cunt4 && cunt2<cunt4 && cunt3<cunt4 && cunt5<cunt4)
-            printf("\t\t\t The present winner is SSD and he got %d votes\n\n\n\n",cunt4);
-
-        if(cunt1<cunt5 && cunt2<cunt5 && cunt3<cunt5 && cunt4<cunt5)
-            printf("\t\t\t The present winner is  and he got %d votes\n\n\n\n",cunt5);
-	if(cunt1==cunt2|| cunt1==cunt3|| cunt1==cunt4|| cunt1==cunt5|| cunt2==cunt3|| cunt2== cunt4|| cunt2== cunt5|| cunt3==cunt4||cunt3==cunt5||cunt4==cunt5)
-	printf("\n\n\n....................*******The election is draw***********.........................");
-
-        int T;
-        printf("\t\t\t\t Enter One(1) For Main Loges OR \n\n\t\t\t\t\t Zero(0) For Exists\n");
-        scanf("%d",&T);
-        if(T==1)
-        {
-
-            main_loges();
-        }
-
-        if(T!=1)
-        {
-
-            exi();
-        }
-
-    }
-
-    void stop()
-    {
-        system("clear");
-
-        printf("\n\n\n\n");
-        printf("\t-----------------------(:- SORRY YOU CAN NOT GIVE YOUR VOTE FOR YOUR WRONG ENTRY THREE(3) TIMES :-)-------------\n\n\n");
-        sleep(5);
-        printf("\t\t\t * * * * ~~~~~~ PLEASE TRY AGAIN AFTER A FEW MOMENT ~~~~~~~ * * * * \n\n\n");
-        sleep(5);
-        printf("\t\t\t\t * * * * * ~~~~~~~~~~~ THANKYOU ~~~~~~~~~~~~~* * * * * *\n\n\n\n");
-        sleep(5);
-
-    }
-    void exi()
-     {
-
-         system("clear");
-         printf("\n\n\n");
-         printf("\t\t -------------- @ YOU HAVE GIVE YOUR VOTE SUCCESSFULLY @ ----------------\n\n\n ");
-         sleep(5);
-         printf("\t\t\t\t\t\t  * * * * * ~~~~~ THANK YOU ~~~~~~  * * * * *\n\n\n\n");
-         sleep(5);
-     }
-
-
-     void not_again()
-     {
-         system("clear");
-         printf("\n\n\n");
-         printf("\t\t\t ---------------------------YOU HAVE GIVE YOUR VOTE SUCCESSFULLY --------------\n\n");
-         sleep(3);
-         printf("           \t\t-------------SO YOU CANNOT GIVE YOUR VOTE MORE THAN ONE(1) TIME ------------\n\n\n");
-         sleep(3);
-         printf("\t\t\t  IF you want to see present winner Enter one (1)  OR \n\t\t\t\t\t\tTwo(2) For Main Loges\n");
-         sleep(3);
-
-         printf("\t\t\t\t\t Zero(0)  For Exists \n\n");
-         scanf("%d",&R);
-         if(R==1)
-         {
-             winner();
-
-         }
-
-         if(R==2)
-         {
-
-             main_loges();
-         }
-         if((R!=1) || (R!=2))
-         {
-
-             exi();
-         }
-
-
-     }
-
-     
-
-
-
-
-
-
-
-
